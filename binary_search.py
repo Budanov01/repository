fa9b0046458a33c8cd667949264e8468b197f4bb
Budanov Aleksandr <budanovaleksandr@gmail.com>
2018-03-09 18:49:27 +0300

    initial commit

---
+++ binary_search.py
@@ -0,0 +1,16 @@
+# coding=utf-8
+A = list(input('введите список '))
+key = input('введите искомый ключ ')
+
+def binary_search(A, key):
+    left = -1
+    right = len(A) 
+    while right > left + 1:
+        middle = (left + right) // 2 
+        if A[middle] >= key: 
+            right = middle 
+        else: 
+            left = middle 
+    return right
+
+print(binary_search(A, key))
