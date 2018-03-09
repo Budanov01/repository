af4affe8ffe8fbb5564e8ecf6cc76309e8a90fdd
Aleksandr <36704763+Budanov01@users.noreply.github.com>
2018-03-09 17:15:54 +0300

    Update Calculation_of__water.py

--- Calculation_of__water.py
+++ Calculation_of__water.py
@@ -4,9 +4,7 @@ water_volume = input('ср.V(л) воды в минуту ')
 
 
 def calculation_of_water(Time, water_volume):
-    if Time < 0 and water_volume < 0:
-        return 'Ошибка, значения не могут быть < 0'
-    elif Time < 0 or water_volume < 0:
+    if Time < 0 or water_volume < 0:
         return 'Ошибка, значение не может быть < 0'
     elif Time - int(Time) > 0 or water_volume - int(water_volume) > 0:
         return 'значения должны быть целочисленными'
